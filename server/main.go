44c83e931974322eaa5b857c8635b78f50a6d1fa
Sergey Malenkov <sm@level.travel>
2018-02-14 11:31:32 +0300

    Initial commit

---
+++ server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("data/templates/main.html")
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t.Execute(rw, nil)
+}
+
+func main() {
+	http.HandleFunc("/", home)
+
+	fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("data/static")))
+	http.Handle("/static/", fileServer)
+
+	log.Fatal(http.ListenAndServe("localhost:3000", nil))
+}
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'server/data/static/build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      }
+    ],
+  },
+}
